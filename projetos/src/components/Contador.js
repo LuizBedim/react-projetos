389eef85ea578b4540cda97710d5fede2c2acbc6
LuizBedim <LuizOMBedim@gmail.com>
2023-04-27 17:10:51 -0300

    adição dos componentes

---
+++ projetos/src/components/Contador.js
@@ -0,0 +1,30 @@
+
+function Contador () {
+    const [frase, setFrase] = useState("Conheça a Fatec");
+  const [substring, setSubstring] = useState("");
+
+  useEffect(() => {
+    let i = 0;
+
+    const intervalId = setInterval(() => {
+      setSubstring(frase.substring(0, i));
+      i++;
+      if (i > frase.length) {
+        i = 0;
+      }
+    }, 200);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [frase]);
+
+  return (
+    <div>
+    <p>Exercício 2 - Letreiro</p>
+      <h3>{substring}</h3>
+    </div>
+  );
+}
+
+export default Contador;
