3a8c3443d84de300ab701af9b8b5a2d777775ff3
LuizBedim <LuizOMBedim@gmail.com>
2023-05-03 12:13:09 -0300

    componentes dos exercicios

--- projetos/src/App.js
+++ projetos/src/App.js
@@ -1,15 +1,16 @@
 import Relogio from "./components/Relogio";
 import Contador from "./components/Contador";
 import Frase from "./components/Frase";
+import Toolbar from "./components/Toolbar";
+import Gallery from "./components/Gallery";
+import Clock from "./components/Clock";
 
 function App() {
-  return (
-    <>
-      <Relogio />
-	  <Frase />
-	  <Contador />
-    </>
-  );
+    return (
+        <>
+            <Clock />
+        </>
+    );
 }
 
 export default App;
